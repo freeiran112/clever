345a983378c0f778239bfbc639c3c10669db31f7
freeiran112 <124540602+freeiran112@users.noreply.github.com>
2023-04-23 03:22:57 +0430

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 设置各变量
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+UUID='4f7ccd1a-9cbf-41f0-84ab-fd90826cdfcb'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
